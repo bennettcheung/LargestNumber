6500054d0f9f4fce9069b083fb78dda27a51a152
Bennett Cheung <bennettcheung@yahoo.com>
2018-06-24 18:07:41 -0700

    Added function findLargestNumber to return the largest number in an array

--- LargestNumber/main.m
+++ LargestNumber/main.m
@@ -8,10 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+static NSNumber *findLargestNumber(NSArray *numberArray) {
+    NSNumber *largestNum = 0;
+    
+    for (NSNumber *num in numberArray) {
+        if (num > largestNum)
+            largestNum = num;
+    }
+    
+    return (largestNum);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSArray *numberArray = @[@3,@15,@1,@9];
+        
+        
+        NSLog(@"The largest number is %@", findLargestNumber(numberArray));
     }
     return 0;
 }
